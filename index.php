0f02e3201361d7e3d8ec051ab2d09a7bb6efcff0
Xavi55 <evinxavier5@gmail.com>
2019-10-08 11:02:39 -0400

    TODO: be able to delete and update new poll options

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php 
+$action = filter_input(INPUT_POST, 'action');
+if ($action == NULL)
+{
+    $action = filter_input(INPUT_GET, 'action');
+    if ($action == NULL)
+    {
+        $action = 'home';
+    }
+}
+//$_SESSION["name"]="Kevin";
+//unset ($_SESSION["cart"]);//=array();
+
+switch($action)
+{
+  case 'home':
+    header('Location: view/poll.html');
+    break;
+
+  case 'login':
+    $pass=($_POST['pass']);
+
+    $file=fopen('model/admin.txt','r');
+    if(fgets($file)===$pass)
+    {
+      echo 1;
+    }
+    fclose($file);
+    break;
+
+  case 'load':
+    echo json_encode(json_decode(file_get_contents('model/currPoll.json'),true));
+    break;
+
+  case 'new':
+  /*
+    $file=fopen('model/currPoll.json','r');
+    fwrite($file,'{}');
+    fclose($file);
+  */
+    break;
+
+
+
+  case 'submit':
+    $data=json_decode(file_get_contents('model/currPoll.json'),true);
+    if(isset($_POST['add']))
+    {
+      $data[$_POST['add']]+=1;
+      $data["total"]+=1;
+    }
+    $data=json_encode($data);
+    file_put_contents('model/currPoll.json',$data);
+    break;
+}
+?>
---
+++ view/poll.js
@@ -0,0 +1,99 @@
+window.onload=()=>
+{
+  loadPoll();
+  $('.btn-group button').removeClass('admin');
+};
+
+function loadPoll()
+{
+  $.get('../index.php',{action:'load'},data=>
+  {
+    data=JSON.parse(data);
+    $('#selection').html('');
+    for(let i in data)
+    {
+      if(i==='total') continue;
+      render(i);
+    }
+
+  });
+}
+
+function render(person)
+{
+  $('#selection').append(`
+      <div class="col-sm-4 col-md-4">
+          <div class="thumbnail">
+            <div id="${person}" class="caption">
+              <h3 class=center>${person}</h3>
+              <p class=center><a href="#/" class="btn btn-default" onclick=pick("${person}")>Vote</a></p>
+            </div>
+          </div>
+        </div>`);
+}
+
+function pick(person)
+{
+  $('.caption a').removeClass('btn-primary');
+  $('.caption h3').removeClass('vote');
+
+  $(`#${person} a`).addClass('btn-primary');
+  $(`#${person} h3`).addClass('vote');
+
+}
+
+function login()
+{
+  let pass=$('#pass').val();
+  $.post('../index.php',{action:"login","pass":pass},data=>
+  {
+    if(data)
+    {
+      $('.btn-group button').removeClass('admin');
+      alert('save after editing.');
+    }
+  });
+}
+
+function submit()
+{
+  let person=$(`.vote`).text();
+  $.post('../index.php',{action:"submit","add":person},data=>
+  {
+    console.log(data);
+  })
+}
+
+function newEntry()
+{
+  $('#selection').append(`
+      <div class="col-sm-4 col-md-4 temp">
+          <div class="thumbnail">
+            <div class="caption">
+             <input type="text" class="form-control add" placeholder="New Entry...">
+            </div>
+          </div>
+        </div>`);
+}
+
+function newPoll()
+{
+
+  let entries = document.getElementsByClassName('add');
+  if(entries.length!=0)
+  {
+    for(let i=0; i<entries.length;i++)
+  {
+    if(entries[i].value)
+    {
+      render(entries[i].value);
+    }
+  }
+  $('.temp ').remove();
+
+  //send new name to php
+
+    //$('.btn-group button').addClass('admin');
+  }
+  
+}
